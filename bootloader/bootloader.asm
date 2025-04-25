0fbeb2b9b1d30a991dbf2c492df2966b8f89b4fe
adam7100 <73548219+adam757521@users.noreply.github.com>
2025-04-25 20:04:08 +0300

    FAT root directory traversal

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -12,25 +12,9 @@ start:
     sub sp, 0x30
     mov bp, sp
 
-    ;mov si, dapack 
-    ;mov ah, 0x42
-    ;mov dl, 0x80
-    ;int 0x13
-
-    mov ah, 8h
-    mov dl, 0x80
-    int 13h
-
-    inc dh
-    and cl, 0x3F
-    xor dl, dl
-    or dl, cl
-    ; first byte - head
-    ; second byte - SPT
-    mov [bp], dx
-
+    ; Read BPB
     mov bx, 0x7E00
-    mov si, 0x1FA2
+    mov si, 0x0
     mov al, 1
     call read_sector_lba
 
@@ -41,102 +25,128 @@ start:
     ; DI: dst, SI: src, CX: iterator
     rep movsb
 
-    ; Calculate root directory LBA
-    ; LBA = Reserved Sectors + sectors per fat * number of fats
-    mov ax, [bp + 0x5 + 0x4] ; number of FAT
-    mov dx, [bp + 0x19 +0x4] ; sectors per FATs
+    ; root dir LBA = Reserved Sectors + sectors per fat * number of fats
+    mov ax, [bp + 0x5] ; number of FAT
+    mov dx, [bp + 0x19] ; sectors per FATs
     mul dx 
-    add ax, [bp + 0x3 +0x4] ; reserved sectors 
+    add ax, [bp + 0x3] ; reserved sectors 
 
-    ;temp - root directories
-    mov bx, [bp + 0x6 + 0x4]
-
-    ;mov cl, ax
+    mov si, ax
     mov bx, 0x7E00
+    ; TODO: root dir is only a single sector
     mov al, 1
-    call read_sector
+    call read_sector_lba
+
+    sub sp, 16
+    mov bp, sp
+    mov bx, 0x7e00
+search_loop:
+    mov al, byte [bx]
+    cmp al, 0
+    je done
+
+    cmp al, 0xE5
+    je next
+
+    cmp byte [bx+11], 0x0f
+    jne sfn
+    
+    mov si, bx
+    inc si
+    mov di, bp
+    mov cx, 5
+    call copy_utf16
+
+    mov si, bx
+    add si, 14
+    mov cx, 6
+    call copy_utf16
+
+    mov si, bx
+    add si, 28
+    mov cx, 2
+    call copy_utf16
+
+    jmp next
+sfn:
+    cmp byte [bp], 0x0
+    jne name_set
+
+    ; TODO: support short file name
+
+name_set:
+    ; check if our file was found
+    ; if not, continue
+    ; if found, is the second found
+    ; can keep count, no duuplicate
+    mov di, kernel_file
+    mov cx, kernel_file_len
+    mov si, bp
+    repe cmpsb
+    jne next
+
+    ;mov dx, [bx + 20]
+    ;mov ax, [bx + 26]
+    mov dx, 0xABCD
+    mov ax, 0x5678
+
+    ; Do you think im happy writing this code? (1 bit carry flag)
+    shl ax, 1
+    rcl dx, 1
+
+    shl ax, 1
+    rcl dx, 1
+
+    xor ax, ax
+
+
+    ;mov ax, [bx + 28]
+
+    ; test if two strings were found
 
     ;gammaaaaaa
-
-    ; Load root directory sectors
-    mov si, 0
-load_root_dir:
-    ;mov ax, [root_dir_lba]
-    add ax, si
-    ;call read_sector
-    inc si
-    ;cmp si, [root_dir_sectors]
-    ;jl load_root_dir
-
-    ; Search for filename "KERNEL  BIN"
-    mov di, 0x7E00              ; start of root dir
-search_loop:
-    mov si, filename
-    push di
-    mov cx, 11
-    repe cmpsb
-    pop di
-    je file_found
-    add di, 32
-    cmp di, 0x7E00 + 512
-    jl search_loop
-
-not_found:
+next:
+    add bx, 32
+    jmp search_loop
+done:
+    add sp, 16
     jmp $
 
-file_found:
-    ; DI points to directory entry
-    mov ax, [di + 26]           ; first cluster
-    ;mov [file_cluster], ax
-    ;cyberrrrrr
-    ; Load cluster into 0x1000
-    mov bx, 0x1000
-    ;call load_cluster
-    jmp 0x1000                  ; jump to kernel
+traverse_fat_copy_file:
+    ; read the cluster to a pointer
+    ; calculate next cluster
+    ; loop
 
-; Converts cluster # in AX to LBA in AX
-cluster_to_lba:
-    sub ax, 2
-    xor dx, dx
-    ;mov dl, [bpb_struct + 13]  ; sectors per cluster
-    mul dx
-    ;add ax, [root_dir_lba]
-    ;add ax, [root_dir_sectors]
-    ret
+; Read(bx dest, si LBA, al sector_count)
+read_sector_lba:
+    mov [dapack+0x8], si
+    mov [dapack+0x2], al
+    mov [dapack+0x4], bx
 
-; Read (bx dest, si lba, al sector_count)
-
-; Read(bx dest, al sector_count, cl start_sector, ch cylinder, dh head)
-read_sector:
-    mov ah, 0x02
+    xor ax, ax
+    mov ds, ax
+    mov si, dapack 
+    mov ah, 0x42
     mov dl, 0x80
     int 0x13
     ret
-
-load_cluster:
-    push ax
-    ;mov ax, [file_cluster]
-    ;call cluster_to_lba
-    ;call read_sector
-    ; Copy from 0x7E00 to 0x1000
-    mov si, 0x7E00
-    mov di, bx
-    mov cx, 512
-    rep movsb
-    pop ax
+; cx count, si source, di dest
+copy_utf16:
+    movsb
+    add si, 1
+    loop copy_utf16
     ret
 
-filename db 'KERNEL  BIN'
-file_cluster dw 0
+kernel_file db 'kernel.bin' 
+kernel_file_len equ $ - kernel_file
+
 dapack:
 db 0x10
 db 0
-dw 16
-dw 0x7E00
 dw 0
-dd 8098
+dw 0xFFFF
+dw 0
+dd 0 
 dd 0
-
 times 510 - ($-$$) db 0
 dw 0xAA55
-
